0ac2afab015ac1a42afe4c5ef8a30f2637d517a5
Trishant Verma <97427506+TRISHANT99@users.noreply.github.com>
2022-05-17 15:23:43 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-let cmbtn = document.getElementById("cm");
-cmbtn.addEventListener('click', function () {
-    let input = document.getElementById("input").value;
-    document.getElementById("output").value = input*30.48 +" "+ "cm";
-});
